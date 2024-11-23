44c260c32ffed25ea85f7facc81f8b05fadf2b68
Sayan Sengupta <subhosen16@gmail.com>
2024-11-23 19:59:55 +0530

    Committing the Changes as of 23-11-2024 (Transforming Api Response)

--- springbootweb/springbootweb/src/main/java/com/codingshuttle/springbootweb/advices/GlobalExceptionHandler.java
+++ springbootweb/springbootweb/src/main/java/com/codingshuttle/springbootweb/advices/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.codingshuttle.springbootweb.advices;
 
 import com.codingshuttle.springbootweb.exceptions.ResourceNotFoundException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,33 +8,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException exception){
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFoundException(ResourceNotFoundException exception){
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .messege(exception.getMessage())
                 .build();
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        return buildErrorResponseBody(apiError);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleInternalServerErrors(Exception exception){
+    public ResponseEntity<ApiResponse<?>> handleInternalServerErrors(Exception exception){
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .messege(exception.getMessage())
                 .build();
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildErrorResponseBody(apiError);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleInternalServerErrors(MethodArgumentNotValidException exception){
+    public ResponseEntity<ApiResponse<?>> handleInternalServerErrors(MethodArgumentNotValidException exception){
         Map<String,String> fieldErrorMap = new LinkedHashMap<>();
        exception.getBindingResult().getFieldErrors().forEach(fieldError ->{
            fieldErrorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
@@ -45,6 +41,10 @@ public class GlobalExceptionHandler {
                 .messege("Please note the errors in the following fields : ")
                 .subErrors(fieldErrorMap)
                 .build();
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        return buildErrorResponseBody(apiError);
+    }
+
+    private ResponseEntity<ApiResponse<?>> buildErrorResponseBody(ApiError apiError) {
+        return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
     }
 }

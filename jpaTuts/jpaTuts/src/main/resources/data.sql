5bd7a50941755f8c2d9f1c72e262dc66ad8720c1
Sayan Sengupta <subhosen16@gmail.com>
2024-11-24 21:15:09 +0530

    Committing Changes for Spring Data JPA 3.2

---
+++ jpaTuts/jpaTuts/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into product_table (sku,product_name,price)
+values
+	("brit001","Britannia Bourban",25.25),
+	("brit021","Britannia Good Day",20.25),
+	("brit123","Britannia Rusk",50.50),
+	("pepsi123","Pepsi Regular",40.50),
+	("pepsi234","Mirinda",50.50);
---
+++ jpaTuts/jpaTuts/src/test/java/com/codingshuttle/jpa/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
